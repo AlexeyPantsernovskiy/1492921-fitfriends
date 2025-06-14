c507d5a99ba03aee8d0f1894ee2e02e5908ed1fe
Алексей Панцерновский <pan_alexey@mail.ru>
2025-06-15 05:32:28 +0600

    Удалил не используемый код

--- libs/trainings/training-comment/src/training-comment-module/dto/create-comment.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ICreateCommentDto } from '@project/shared-core';
-import { OmitType } from '@nestjs/swagger';
-import { BaseCommentDto } from './base-comment.dto';
-
-export class CreateCommentDto
-  extends OmitType(BaseCommentDto, ['id'])
-  implements ICreateCommentDto {}
